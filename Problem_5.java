10c2b04edbc67c757fa2af335618b2601c5df00d
mkg33 <maja.k.gwozdz@gmail.com>
2018-03-06 23:32:21 +0100

    Problem_5

---
+++ Problem_5.java
@@ -0,0 +1,21 @@
+
+public class Problem_5 {
+	
+	static int result;
+	static boolean divisible;
+	public static void main(String[] args){
+		for(int i=0; i==result; i++){
+			
+			result++;
+		
+			
+		
+			if(result%1==0&&result%2==0&&result%3==0&&result%4==0&&result%5==0&&result%6==0&&result%7==0&&result%8==0&&result%9==0&&result%10==0&&result%11==0&&result%12==0&&result%13==0&&result%14==0&&result%15==0&&result%16==0&&result%17==0&&result%18==0&&result%19==0&&result%20==0){
+				System.out.println(result);
+				break;
+			}
+			
+		}
+	}
+
+}
